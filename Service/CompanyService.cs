2534a78841977f2db404418dc911a4d87d82f681
Eddington <eampofu@gmail.com>
2024-04-19 18:01:41 +0200

    setting up companies controller and getting results from database

--- Service/CompanyService.cs
+++ Service/CompanyService.cs
@@ -1,4 +1,5 @@
 ﻿using Contracts;
+using Entities.Models;
 using Service.Contracts;
 using System;
 using System.Collections.Generic;
@@ -18,6 +19,22 @@ namespace Service
             _repository = repository;
             _logger = logger;
         }
+
+        public IEnumerable<Company> GetAllCompanies(bool trackChanges)
+        {
+            try
+            {
+                var companies =
+                _repository.Company.GetAllCompanies(trackChanges);
+                return companies;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Something went wrong in the {nameof(GetAllCompanies)} service method {ex} ");
+                throw;
+            }
+        }
+
     }
 
 }
